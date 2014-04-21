42b03f251936dcf1be4f47ed03c474007248c04f
Eduardo Castro <eduardocastro91@gmail.com>
2014-04-21 01:35:23 -0300

    Translated perfil.xml to profile.xml

--- APP/gen/com/mds/app/R.java
+++ APP/gen/com/mds/app/R.java
@@ -330,7 +330,7 @@ containing a value of this type.
     public static final class menu {
         public static final int main_menu=0x7f0a0000;
         public static final int menu_about=0x7f0a0001;
-        public static final int perfil=0x7f0a0002;
+        public static final int profile=0x7f0a0002;
     }
     public static final class string {
         public static final int TextoBusca=0x7f070024;
