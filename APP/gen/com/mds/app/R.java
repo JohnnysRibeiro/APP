2605b4f2ef10a35255275ecd0163492087da9cb4
Eduardo Castro <eduardocastro91@gmail.com>
2014-04-21 01:35:54 -0300

    Renamed dimens.xml to dimensions.xml

--- APP/gen/com/mds/app/R.java
+++ APP/gen/com/mds/app/R.java
@@ -165,11 +165,11 @@ containing a value of this type.
         public static final int com_facebook_usersettingsfragment_not_connected_text_color=0x7f060005;
     }
     public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
+        /** 
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
+ Default screen margins, per the Android Design guidelines. 
          */
         public static final int activity_horizontal_margin=0x7f05000d;
         public static final int activity_vertical_margin=0x7f05000e;
