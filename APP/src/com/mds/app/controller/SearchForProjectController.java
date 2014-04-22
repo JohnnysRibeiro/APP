a2d0d577fc8552514154cb144a28c8f5a4229e50
LucianoAlmeida <mdiebr@gmail.com>
2014-04-21 21:52:31 -0300

    Adding a header commentary for the clas SearchForProjectController.java

--- APP/src/com/mds/app/controller/SearchForProjectController.java
+++ APP/src/com/mds/app/controller/SearchForProjectController.java
@@ -1,3 +1,17 @@
+/* File: SearchForProjectController.java
+ * 
+ * Package: com.mds.app.controller
+ * 
+ * Description: This is a controller class for the project search
+ * 
+ * Author: 
+ * 
+ * Creation date:
+ * 
+ * Version: 2.0
+ * 
+ */
+
 package com.mds.app.controller;
 
 import com.mds.app.model.SearchForProjectModel;
