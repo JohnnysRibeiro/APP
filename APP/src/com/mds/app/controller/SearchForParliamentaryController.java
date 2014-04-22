2b371094954b7a50f2af2722f96a59f67fcbd896
LucianoAlmeida <mdiebr@gmail.com>
2014-04-21 21:51:15 -0300

    Adding a header commentary for the clas SearchForParliamentayController.java

--- APP/src/com/mds/app/controller/SearchForParliamentaryController.java
+++ APP/src/com/mds/app/controller/SearchForParliamentaryController.java
@@ -1,3 +1,17 @@
+/* File: SearchForParlamentayController.java
+ * 
+ * Package: com.mds.app.controller
+ * 
+ * Description: This is a controller class for the parliamentary search
+ * 
+ * Author: 
+ * 
+ * Creation date:
+ * 
+ * Version: 2.0
+ * 
+ */
+
 package com.mds.app.controller;
 
 import com.mds.app.model.SearchForParliamentaryModel;
