5171a8c77124e3c95c2e499e06907ad4e63bf913
LucianoAlmeida <mdiebr@gmail.com>
2014-04-21 21:49:36 -0300

    Adding a header commentary for the clas ModifyFilesController.java

--- APP/src/com/mds/app/controller/ModifyFilesController.java
+++ APP/src/com/mds/app/controller/ModifyFilesController.java
@@ -1,3 +1,17 @@
+/* File: ModifyFilesController.java
+ * 
+ * Package: com.mds.app.controller
+ * 
+ * Description: This is a controller class to modify project files, such as add, remove, etc.
+ * 
+ * Author: 
+ * 
+ * Creation date:
+ * 
+ * Version: 2.0
+ * 
+ */
+
 package com.mds.app.controller;
 
 import com.mds.app.model.ProjectModel;
