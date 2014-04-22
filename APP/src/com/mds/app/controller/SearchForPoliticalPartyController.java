97756239480de838a873f395f3a65e83cb5a19e3
LucianoAlmeida <mdiebr@gmail.com>
2014-04-21 21:51:51 -0300

    Adding a header commentary for the clas SearchForPoliticalPartyController.java

--- APP/src/com/mds/app/controller/SearchForPoliticalPartyController.java
+++ APP/src/com/mds/app/controller/SearchForPoliticalPartyController.java
@@ -1,3 +1,17 @@
+/* File: SearchForPoliticalPartyController.java
+ * 
+ * Package: com.mds.app.controller
+ * 
+ * Description: This is a controller class for the political party search
+ * 
+ * Author: 
+ * 
+ * Creation date:
+ * 
+ * Version: 2.0
+ * 
+ */
+
 package com.mds.app.controller;
 
 import com.mds.app.model.SearchForPoliticalPartyModel;
