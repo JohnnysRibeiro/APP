8cc702cc4e94c084d0274c70025ffc2e9d647e01
Eduardo Castro <eduardocastro91@gmail.com>
2014-06-02 10:47:34 -0300

    Fixed/Adapted the test cases for ProjectModel Class

--- APP_TESTES/src/com/mds/app/test/ProjectModelTest.java
+++ APP_TESTES/src/com/mds/app/test/ProjectModelTest.java
@@ -7,18 +7,18 @@ import org.junit.Test;
 
 import android.test.AndroidTestCase;
 
-import com.mds.app.model.ParlamentarModel;
-import com.mds.app.model.ProjetoModel;
+import com.mds.app.model.ParliamentaryModel;
+import com.mds.app.model.ProjectModel;
 
-public class ProjetoModelTest extends AndroidTestCase {
+public class ProjectModelTest extends AndroidTestCase {
 
-	private ProjetoModel projetoModel;
-	private ParlamentarModel parlamentarModel;
+	private ProjectModel projetoModel;
+	private ParliamentaryModel parlamentarModel;
 
 	@Before
 	public void setUp() throws Exception {
-		parlamentarModel = new ParlamentarModel();
-		projetoModel = new ProjetoModel("2013", "NomeProjeto", "PL", "16/10/2013", "66", "ExplicacaoProjeto",
+		parlamentarModel = new ParliamentaryModel();
+		projetoModel = new ProjectModel("2013", "NomeProjeto", "PL", "16/10/2013", "66", "ExplicacaoProjeto",
 				parlamentarModel);
 	}
 
@@ -45,79 +45,79 @@ public class ProjetoModelTest extends AndroidTestCase {
 
 	@Test
 	public void testGetNome() {
-		assertEquals("NomeProjeto", projetoModel.getNome());
+		assertEquals("NomeProjeto", projetoModel.getName());
 	}
 
 	@Test
 	public void testGetAno() {
-		assertEquals("2013", projetoModel.getAno());
+		assertEquals("2013", projetoModel.getYear());
 	}
 
 	@Test
 	public void testGetSigla() {
-		assertEquals("PL", projetoModel.getSigla());
+		assertEquals("PL", projetoModel.getKindOfProjectAcronym());
 	}
 
 	@Test
 	public void testGetData() {
-		assertEquals("16/10/2013", projetoModel.getData());
+		assertEquals("16/10/2013", projetoModel.getDate());
 	}
 
 	@Test
 	public void testGetNumero() {
-		assertEquals("66", projetoModel.getNumero());
+		assertEquals("66", projetoModel.getNumber());
 	}
 
 	@Test
 	public void testGetExplicacao() {
-		assertEquals("ExplicacaoProjeto", projetoModel.getExplicacao());
+		assertEquals("ExplicacaoProjeto", projetoModel.getExplanation());
 	}
 
 	@Test
 	public void testGetParlamentar() {
 		/* ParlamentarModel outroParlamentar = new ParlamentarModel(); */
-		assertEquals(parlamentarModel, projetoModel.getParlamentar());
+		assertEquals(parlamentarModel, projetoModel.getParliamentary());
 	}
 
 	@Test
 	public void testGetContEqualsOne() {
-		assertEquals(1, projetoModel.getCont());
+		assertEquals(1, projetoModel.getCounter());
 	}
 
 	@Test
 	public void testGetContMaiorQueUm() {
-		projetoModel.setNome("TesteCont");
-		assertEquals(2, projetoModel.getCont());
+		projetoModel.setName("TesteCont");
+		assertEquals(2, projetoModel.getCounter());
 	}
 
 	@Test
 	public void testSetNome() {
-		projetoModel.setNome("SetNomeProjeto");
-		assertEquals("SetNomeProjeto", projetoModel.getNome());
+		projetoModel.setName("SetNomeProjeto");
+		assertEquals("SetNomeProjeto", projetoModel.getName());
 	}
 
 	@Test
 	public void testSetAno() {
-		projetoModel.setAno("2012");
-		assertEquals("2012", projetoModel.getAno());
+		projetoModel.setYear("2012");
+		assertEquals("2012", projetoModel.getYear());
 	}
 
 	@Test
 	public void testSetSigla() {
-		projetoModel.setSigla("PDS");
-		assertEquals("PDS", projetoModel.getSigla());
+		projetoModel.setKindOfProjectAcronym("PDS");
+		assertEquals("PDS", projetoModel.getKindOfProjectAcronym());
 	}
 
 	@Test
 	public void testSetData() {
-		projetoModel.setData("15/10/2013");
-		assertEquals("15/10/2013", projetoModel.getData());
+		projetoModel.setDate("15/10/2013");
+		assertEquals("15/10/2013", projetoModel.getDate());
 	}
 
 	@Test
 	public void testSetNumero() {
-		projetoModel.setNumero("67");
-		assertEquals("67", projetoModel.getNumero());
+		projetoModel.setNumber("67");
+		assertEquals("67", projetoModel.getNumber());
 	}
 	
 	@Test
@@ -129,20 +129,20 @@ public class ProjetoModelTest extends AndroidTestCase {
 
 	@Test
 	public void testSetExplicacao() {
-		projetoModel.setExplicacao("SetExplicacaoProjeto");
-		assertEquals("SetExplicacaoProjeto", projetoModel.getExplicacao());
+		projetoModel.setExplanation("SetExplicacaoProjeto");
+		assertEquals("SetExplicacaoProjeto", projetoModel.getExplanation());
 	}
 
 	@Test
 	public void testSetParlamentar() {
-		ParlamentarModel outroParlamentar = new ParlamentarModel();
-		projetoModel.setParlamentar(outroParlamentar);
-		assertEquals(outroParlamentar, projetoModel.getParlamentar());
+		ParliamentaryModel outroParlamentar = new ParliamentaryModel();
+		projetoModel.setParliamentary(outroParlamentar);
+		assertEquals(outroParlamentar, projetoModel.getParliamentary());
 	}
 
 	@Test
 	public void testToString() {
-		ProjetoModel outroProjetoModel = new ProjetoModel("2013", "NomeProjeto", "PL", "16/10/2013", "66",
+		ProjectModel outroProjetoModel = new ProjectModel("2013", "NomeProjeto", "PL", "16/10/2013", "66",
 				"ExplicacaoProjeto", parlamentarModel);
 		assertEquals(projetoModel.toString(), outroProjetoModel.toString());
 	}
@@ -158,41 +158,41 @@ public class ProjetoModelTest extends AndroidTestCase {
 	@Test
 	public void testGetContId() {
 		projetoModel.setId("qualquer coisa");
-		assertEquals(1, projetoModel.getContId());
+		assertEquals(1, projetoModel.getCounterId());
 	}
 
 	@Test
 	public void testSetContId() {
 		int esperado = 3;
-		projetoModel.setContId(esperado);
-		assertEquals(esperado, projetoModel.getContId());
+		projetoModel.setCounterId(esperado);
+		assertEquals(esperado, projetoModel.getCounterId());
 	}
 
 	@Test
 	public void testSetCont() {
 		int esperado = 5;
-		projetoModel.setCont(esperado);
-		assertEquals(esperado, projetoModel.getCont());
+		projetoModel.setCounter(esperado);
+		assertEquals(esperado, projetoModel.getCounter());
 	}
 
 	@Test
 	public void testConstrutorVazioNaoNulo() {
-		ProjetoModel projetoVazio = new ProjetoModel();
+		ProjectModel projetoVazio = new ProjectModel();
 		assertNotNull(projetoVazio);
 	}
 
 	@Test
 	public void testSetIdComContIdMaiorQueZero() {
-		projetoModel.setContId(3);
+		projetoModel.setCounterId(3);
 		projetoModel.setId("IDZORDON");
-		assertEquals(3, projetoModel.getContId());
+		assertEquals(3, projetoModel.getCounterId());
 	}
 
 	@Test
 	public void testSetNomeComContMaiorQueUm() {
-		projetoModel.setCont(5);
-		projetoModel.setNome("NOMEZORDON");
-		assertEquals(6, projetoModel.getCont());
+		projetoModel.setCounter(5);
+		projetoModel.setName("NOMEZORDON");
+		assertEquals(6, projetoModel.getCounter());
 	}	
 
 }
