f4d9ff4630cb072c32554cbdb264e38699729857
Eduardo Castro <eduardocastro91@gmail.com>
2014-06-02 20:39:02 -0300

    Translated and improved the names used on FavoritesControllerTest. All the tests keeps green

--- APP_TESTES/src/com/mds/app/test/FavoritesControllerTest.java
+++ APP_TESTES/src/com/mds/app/test/FavoritesControllerTest.java
@@ -17,92 +17,92 @@ import com.mds.app.model.ProjectModel;
 
 public class FavoritesControllerTest extends AndroidTestCase {
 
-	public FavoritesController favoritosController;
-	public ArrayList<String> projetosString;
-	public ArrayList<ProjectModel> projetos;
-	public ProjectModel projetoModel;
-	public ParliamentaryModel parlamentarModel;
-	public PoliticalPartyModel partidoModel;
+	public FavoritesController favoritesController;
+	public ArrayList<String> arrayOfStringsOfProjects;
+	public ArrayList<ProjectModel> projects;
+	public ProjectModel projectModel;
+	public ParliamentaryModel parliamentaryModel;
+	public PoliticalPartyModel politicalPartyModel;
 
 	@Before
 	public void setUp() throws Exception {
-		projetosString = new ArrayList<String>();
-		projetos = new ArrayList<ProjectModel>();
-		partidoModel = new PoliticalPartyModel("PMDS", "AC");
-		parlamentarModel = new ParliamentaryModel("Ranger", partidoModel);
-		projetoModel = new ProjectModel("2013", "Zordon", "PL", "12/01/2013", "6663", "explicacao marota",
-				parlamentarModel);
-		projetos.add(projetoModel);
+		arrayOfStringsOfProjects = new ArrayList<String>();
+		projects = new ArrayList<ProjectModel>();
+		politicalPartyModel = new PoliticalPartyModel("PMDS", "AC");
+		parliamentaryModel = new ParliamentaryModel("Ranger", politicalPartyModel);
+		projectModel = new ProjectModel("2013", "Zordon", "PL", "12/01/2013", "6663", "explicacao marota",
+				parliamentaryModel);
+		projects.add(projectModel);
 
-		favoritosController = new FavoritesController();
-		FavoritesController.setFavoritedProjects(projetos);
+		favoritesController = new FavoritesController();
+		FavoritesController.setFavoritedProjects(projects);
 		FavoritesController.setFavoritedProjectsCompleteString(new ArrayList<String>());
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		favoritosController = null;
-		projetosString = null;
-		projetos = null;
-		projetoModel = null;
-		parlamentarModel = null;
-		partidoModel = null;
+		favoritesController = null;
+		arrayOfStringsOfProjects = null;
+		projects = null;
+		projectModel = null;
+		parliamentaryModel = null;
+		politicalPartyModel = null;
 		FavoritesController.setFavoritedProjects(null);
 		FavoritesController.setFavoritedProjects(null);
 	}
 
 	@Test
 	public void testInstance() {
-		assertNotNull(favoritosController);
+		assertNotNull(favoritesController);
 	}
 
 	@Test
 	public void testProjetosEmString() {
-		favoritosController.populateListWithProjects();
-		String esperado = "Zordon\nNumero: 6663\nAno: 2013\nSigla: PL\nData de Apresentacao: 12/01/2013\nDescricao: explicacao marota\nParlamentar: Ranger\nPartido: PMDS\nEstado: AC";
-		String retornado = favoritosController.transformProjectsIntoString();
-		assertEquals(esperado, retornado);
+		favoritesController.populateListWithProjects();
+		String expectedReturn = "Zordon\nNumero: 6663\nAno: 2013\nSigla: PL\nData de Apresentacao: 12/01/2013\nDescricao: explicacao marota\nParlamentar: Ranger\nPartido: PMDS\nEstado: AC";
+		String actualReturn = favoritesController.transformProjectsIntoString();
+		assertEquals(expectedReturn, actualReturn);
 	}
 
 	@Test
 	public void testPopularListaComProjetos() {
-		FavoritesController.setFavoritedProjects(projetos);
-		favoritosController.populateListWithProjects();
-		String esperado = "[Zordon\nNumero: 6663\nAno: 2013\nSigla: PL\nData de Apresentacao: 12/01/2013\nDescricao: explicacao marota\nParlamentar: Ranger\nPartido: PMDS\nEstado: AC]";
-		String retornado = FavoritesController.getFavoritedProjectsCompleteString().toString();
-		assertEquals(esperado, retornado);
+		FavoritesController.setFavoritedProjects(projects);
+		favoritesController.populateListWithProjects();
+		String expectedReturn = "[Zordon\nNumero: 6663\nAno: 2013\nSigla: PL\nData de Apresentacao: 12/01/2013\nDescricao: explicacao marota\nParlamentar: Ranger\nPartido: PMDS\nEstado: AC]";
+		String actualReturn = FavoritesController.getFavoritedProjectsCompleteString().toString();
+		assertEquals(expectedReturn, actualReturn);
 	}
 
 	@Test
 	public void testPopularProjetos() {
 		ListController listaController = new ListController();
-		ListController.setActualProject(projetoModel);
-		String stringInput = listaController.getCompleteStringForAFile();
-		favoritosController.populateProjects(stringInput);
-		String esperado = FavoritesController.getFavoritedProjects().get(0).toString();
-		String retornado = projetoModel.toString();
-		assertEquals(esperado, retornado);
+		ListController.setActualProject(projectModel);
+		String inputString = listaController.getCompleteStringForAFile();
+		favoritesController.populateProjects(inputString);
+		String expectedReturn = FavoritesController.getFavoritedProjects().get(0).toString();
+		String actualReturn = projectModel.toString();
+		assertEquals(expectedReturn, actualReturn);
 	}
 
 	@Test
 	public void testSetThenGetProjetosFavoritadosCompletosStr() {
-		ArrayList<String> arrayTeste = new ArrayList<String>();
-		arrayTeste.add("TESTANDO PROJETO");
-		FavoritesController.setFavoritedProjectsCompleteString(arrayTeste);
-		assertSame(arrayTeste, FavoritesController.getFavoritedProjectsCompleteString());
+		ArrayList<String> arrayForTestingPurposes = new ArrayList<String>();
+		arrayForTestingPurposes.add("TESTANDO PROJETO");
+		FavoritesController.setFavoritedProjectsCompleteString(arrayForTestingPurposes);
+		assertSame(arrayForTestingPurposes, FavoritesController.getFavoritedProjectsCompleteString());
 	}
 
 	@Test
 	public void testSetThenGetProjetosFavoritados() {
-		FavoritesController.setFavoritedProjects(projetos);
-		assertSame(projetos, FavoritesController.getFavoritedProjects());
+		FavoritesController.setFavoritedProjects(projects);
+		assertSame(projects, FavoritesController.getFavoritedProjects());
 	}
 
 	@Test
 	public void testarNomeDaClasse() {
-		String nomeEsperado = "FavoritesController";
-		String nomeRetornado = favoritosController.getClass().getSimpleName();
-		assertEquals(nomeEsperado, nomeRetornado);
+		String expectedNameReturn = "FavoritesController";
+		String actualNameReturn = favoritesController.getClass().getSimpleName();
+		assertEquals(expectedNameReturn, actualNameReturn);
 	}
 
 }
