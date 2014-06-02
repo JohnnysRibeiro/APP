fa97200cffd5358fc4696da1bc24cd5748e5865f
Eduardo Castro <eduardocastro91@gmail.com>
2014-06-02 09:05:00 -0300

    Fixed/Adapted the test cases for ListController Class

--- APP_TESTES/src/com/mds/app/test/ListControllerTest.java
+++ APP_TESTES/src/com/mds/app/test/ListControllerTest.java
@@ -8,31 +8,31 @@ import org.junit.Test;
 
 import android.test.AndroidTestCase;
 
-import com.mds.app.controller.ListaController;
-import com.mds.app.model.ParlamentarModel;
-import com.mds.app.model.PartidoModel;
-import com.mds.app.model.ProjetoModel;
+import com.mds.app.controller.ListController;
+import com.mds.app.model.ParliamentaryModel;
+import com.mds.app.model.PoliticalPartyModel;
+import com.mds.app.model.ProjectModel;
 
-public class ListaControllerTest extends AndroidTestCase {
+public class ListControllerTest extends AndroidTestCase {
 
-	public ListaController listaController;
+	public ListController listaController;
 	public ArrayList<String> stringLista;
-	public ArrayList<ProjetoModel> projetos;
-	public ProjetoModel projetoModel;
-	public ParlamentarModel parlamentarModel;
-	public PartidoModel partidoModel;
+	public ArrayList<ProjectModel> projetos;
+	public ProjectModel projetoModel;
+	public ParliamentaryModel parlamentarModel;
+	public PoliticalPartyModel partidoModel;
 
 	@Before
 	public void setUp() throws Exception {
 		stringLista = new ArrayList<String>();
-		projetos = new ArrayList<ProjetoModel>();
-		partidoModel = new PartidoModel("PMDS", "AC");
-		parlamentarModel = new ParlamentarModel("Ranger", partidoModel);
-		projetoModel = new ProjetoModel("2013", "Zordon", "PL", "12/01/2013", "6663", "explicacao marota",
+		projetos = new ArrayList<ProjectModel>();
+		partidoModel = new PoliticalPartyModel("PMDS", "AC");
+		parlamentarModel = new ParliamentaryModel("Ranger", partidoModel);
+		projetoModel = new ProjectModel("2013", "Zordon", "PL", "12/01/2013", "6663", "explicacao marota",
 				parlamentarModel);
 		projetos.add(projetoModel);
 
-		listaController = new ListaController(projetos);
+		listaController = new ListController(projetos);
 	}
 
 	@After
@@ -52,13 +52,13 @@ public class ListaControllerTest extends AndroidTestCase {
 
 	@Test
 	public void testInstanceConstrutorVazio() {
-		ListaController listaControllerTeste = new ListaController();
+		ListController listaControllerTeste = new ListController();
 		assertNotNull(listaControllerTeste);
 	}
 
 	@Test
 	public void testTransformarLista() {
-		ArrayList<String> retornado = listaController.transformarLista();
+		ArrayList<String> retornado = listaController.transformAProjectListIntoAnArrayList();
 
 		String stringEsperada = "[Zordon - 6663 - Ranger]";
 		String stringRetornada = retornado.toString();
@@ -68,32 +68,32 @@ public class ListaControllerTest extends AndroidTestCase {
 
 	@Test
 	public void testGetListaProjetos() {
-		assertEquals(projetos, ListaController.getListaProjetos());
+		assertEquals(projetos, ListController.getProjectsList());
 	}
 
 	@Test
 	public void testSetListaProjetos() {
-		ArrayList<ProjetoModel> novoProjetos = new ArrayList<ProjetoModel>();
-		ProjetoModel novoProjetoModel = new ProjetoModel("2012", "DIFF", "PEC", "12/01/2013", "6263",
+		ArrayList<ProjectModel> novoProjetos = new ArrayList<ProjectModel>();
+		ProjectModel novoProjetoModel = new ProjectModel("2012", "DIFF", "PEC", "12/01/2013", "6263",
 				"explicacao", parlamentarModel);
 		novoProjetos.add(novoProjetoModel);
-		ListaController.setListaProjetos(novoProjetos);
-		assertSame(novoProjetos, ListaController.getListaProjetos());
+		ListController.setProjectsList(novoProjetos);
+		assertSame(novoProjetos, ListController.getProjectsList());
 	}
 
 	@Test
 	public void testSetThenGetProjetoAtual() {
-		ProjetoModel projetoModel2 = new ProjetoModel("2012", "nomedois", "PRC", "12/01/2013", "6263",
+		ProjectModel projetoModel2 = new ProjectModel("2012", "nomedois", "PRC", "12/01/2013", "6263",
 				"explicacao marota", parlamentarModel);
-		ListaController.setProjetoAtual(projetoModel2);
-		assertSame(projetoModel2, ListaController.getProjetoAtual());
+		ListController.setActualProject(projetoModel2);
+		assertSame(projetoModel2, ListController.getActualProject());
 	}
 
 	@Test
 	public void testTransformarListaNull() {
-		ListaController listaControllerTeste = new ListaController();
-		ListaController.setListaProjetos(null);
-		ArrayList<String> arrayRetornado = listaControllerTeste.transformarLista();
+		ListController listaControllerTeste = new ListController();
+		ListController.setProjectsList(null);
+		ArrayList<String> arrayRetornado = listaControllerTeste.transformAProjectListIntoAnArrayList();
 		String esperado = "[Nada encontrado.]";
 		String retornado = arrayRetornado.toString();
 		assertEquals(esperado, retornado);
@@ -102,24 +102,24 @@ public class ListaControllerTest extends AndroidTestCase {
 
 	@Test
 	public void testGetStringCompletaParaPerfil() {
-		ListaController.setProjetoAtual(projetoModel);
-		String retornado = listaController.getStringCompletaParaPerfil();
+		ListController.setActualProject(projetoModel);
+		String retornado = listaController.getCompleteStringForProfile();
 		String esperado = "Zordon\nNumero: 6663\nAno:  2013\nSigla: PL\nData de Apresentação: 12/01/2013\nDescrição: explicacao marota\nParlamentar: Ranger\nPartido: PMDS\nEstado: AC";
 		assertEquals(esperado, retornado);
 	}
 
 	@Test
 	public void testGetStringCompletaParaPerfilNull() {
-		ListaController.setProjetoAtual(null);
-		String retornado = listaController.getStringCompletaParaPerfil();
+		ListController.setActualProject(null);
+		String retornado = listaController.getCompleteStringForProfile();
 		String esperado = "Nada encontrado.";
 		assertEquals(esperado, retornado);
 	}
 
 	@Test
 	public void testGetStringCompletaParaArquivo() {
-		ListaController.setProjetoAtual(projetoModel);
-		String retornado = listaController.getStringCompletaParaArquivo();
+		ListController.setActualProject(projetoModel);
+		String retornado = listaController.getCompleteStringForAFile();
 		System.out.println(retornado);
 		String esperado = "Zordon~6663~2013~PL~12/01/2013~explicacao marota~Ranger~PMDS~AC~";
 		assertEquals(esperado, retornado);
@@ -127,8 +127,8 @@ public class ListaControllerTest extends AndroidTestCase {
 
 	@Test
 	public void testGetStringCompletaParaArquivoNull() {
-		ListaController.setProjetoAtual(null);
-		String retornado = listaController.getStringCompletaParaArquivo();
+		ListController.setActualProject(null);
+		String retornado = listaController.getCompleteStringForAFile();
 		String esperado = null;
 		assertEquals(esperado, retornado);
 	}
@@ -136,7 +136,7 @@ public class ListaControllerTest extends AndroidTestCase {
 	@Test
 	public void testGetSeparador() {
 		String esperado = "~";
-		String retornado = ListaController.getSeparador();
+		String retornado = ListController.getSeparator();
 		assertEquals(esperado, retornado);
 	}
 
