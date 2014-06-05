099d7b401b6d823863043caa3a012f3525517174
Eduardo Castro <eduardocastro91@gmail.com>
2014-06-05 11:49:28 -0300

    Fixed tome tests on SearchForProjectModelTest and translated and improved some names on this class. All tests are green

--- APP_TESTES/src/com/mds/app/test/SearchForProjectModelTest.java
+++ APP_TESTES/src/com/mds/app/test/SearchForProjectModelTest.java
@@ -10,44 +10,44 @@ import com.mds.app.model.SearchForProjectModel;
 
 public class SearchForProjectModelTest extends AndroidTestCase {
 
-	private SearchForProjectModel procuraProjetoModel;
+	private SearchForProjectModel searchForProjectModel;
 
 	@Before
 	public void setUp() throws Exception {
-		procuraProjetoModel = new SearchForProjectModel() {
+		searchForProjectModel = new SearchForProjectModel() {
 		};
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		procuraProjetoModel = null;
+		searchForProjectModel = null;
 	}
 
 	@Test
 	public void testIntanciaProjeto() {
-		assertNotNull(procuraProjetoModel);
+		assertNotNull(searchForProjectModel);
 	}
 
 	@Test
-	public void testSetThenGetAno() {
+	public void testSetTheYearForASearchAndGetItBack() {
 		SearchForProjectModel.setYear("2013");
 		assertEquals("2013", SearchForProjectModel.getYear());
 	}
 
 	@Test
-	public void testSetThenGetDataInicio() {
+	public void testSetTheInitialDateForASearchAndGetItBack() {
 		SearchForProjectModel.setInitialDate("16/10/2013");
 		assertEquals("16/10/2013", SearchForProjectModel.getInitialDate());
 	}
 
 	@Test
-	public void testSetThenGetId() {
+	public void testSetTheNumberIdForASearchAndGetItBack() {
 		SearchForProjectModel.setId("99");
 		assertEquals("99", SearchForProjectModel.getId());
 	}
 
 	@Test
-	public void testSetThenGetNome() {
+	public void testSetTheKindOfProjectAcronymForASearchAndGetItBack() {
 		SearchForProjectModel.setKindOfProjectAcronym("PL");
 		assertEquals("PL", SearchForProjectModel.getKindOfProjectAcronym());
 	}
