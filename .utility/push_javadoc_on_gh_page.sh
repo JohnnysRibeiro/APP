df31f6f2067bc7db803baa5710a2ef060d550c67
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2014-12-08 23:24:25 -0200

    Putting the right repository on the script

--- .utility/push_javadoc_on_gh_page.sh
+++ .utility/push_javadoc_on_gh_page.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_REPO_SLUG" == "ReadyTalk/swt-bling" ] && [ "$TRAVIS_PULL_REQUEST" 
 	cd $HOME
 	git config --global user.email "travis@travis-ci.org"
 	git config --global user.name "travis-ci"
-	git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ReadyTalk/swt-bling gh-pages > /dev/null
+	git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/JohnnysRibeiro/APP gh-pages > /dev/null
 	cd gh-pages
 	git rm -rf ./javadoc
 	cp -Rf $HOME/javadoc-latest ./javadoc
