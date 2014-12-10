24448570ee0be45d0cf475fe4aef86157ba0c664
Caio Nardelli <caio.nardelli@hotmail.com>
2014-12-10 17:31:03 -0200

    Trying to fix anonymous access denied from travis

--- push_javadoc_to_gh_page.sh
+++ push_javadoc_to_gh_page.sh
@@ -1,8 +1,8 @@
-echo -e "Starting JavaDoc publish script."
+echo -e "## Starting JavaDoc publish script."
 
 if [ "$TRAVIS_REPO_SLUG" == "JohnnysRibeiro/APP" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
-echo -e "Publishing javadoc...\n"
+echo -e "## Publishing javadoc...\n"
 cp -R ./javadoc/ $HOME/javadoc-latest
 cd $HOME
 git config --global user.email "travis@travis-ci.org"
@@ -13,11 +13,15 @@ git rm -rf ./javadoc
 cp -Rf $HOME/javadoc-latest ./javadoc
 git add -f .
 git commit -m "Lastest javadoc on travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+echo -e "## Changing remote origin"
+git remote rm origin
+git remote add origin https://JohnnysRibeiro:${GH_TOKEN}@github.com/JohnnysRibeiro/APP.git
+echo -e "## Attempting a push"
 git push -fq origin gh-pages > /dev/null
-echo -e "Published Javadoc to gh-pages.\n"
+echo -e "## Published Javadoc to gh-pages.\n"
 
 else
 
-echo -e "Could not publish JavaDoc."
+echo -e "## Could not publish JavaDoc."
 
 fi
