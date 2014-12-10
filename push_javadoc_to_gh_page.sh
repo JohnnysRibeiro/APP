debd434e8c8b9dfa945aef8551dbd0af1dd538b6
CaioIcy <caio.nardelli@hotmail.com>
2014-12-10 17:08:56 -0200

    Fixing some javadoc mistakes

--- push_javadoc_to_gh_page.sh
+++ push_javadoc_to_gh_page.sh
@@ -3,7 +3,7 @@ echo -e "Starting JavaDoc publish script."
 if [ "$TRAVIS_REPO_SLUG" == "JohnnysRibeiro/APP" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
 echo -e "Publishing javadoc...\n"
-cp -R build/docs/javadoc $HOME/javadoc-latest
+cp -R ./javadoc/ $HOME/javadoc-latest
 cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
